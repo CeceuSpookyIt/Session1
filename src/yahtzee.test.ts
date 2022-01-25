34b508872bd5e4f664040b530e3194d74c02c4ef
Marcel Cruz <marcelcruz@outlook.com>
2022-01-24 21:20:22 -0500

    Parabens aos envolvidos
    Co-authored-by: Rodrigo Tardeo <DigoTardeo@users.noreply.github.com>
    Co-authored-by: Nyronmi <Nyronmi@users.noreply.github.com>
    Co-authored-by: Guilherme Carneiro <gcomartins@gmail.com>

--- src/yahtzee.test.ts
+++ src/yahtzee.test.ts
@@ -2,56 +2,102 @@ import { Yahtzee, Regra } from "./yahtzee";
 
 describe("Testes Tradutor Numero Romano", () => {
   let _sut: Yahtzee;
-  
-  beforeEach(() => {
-    _sut = new Yahtzee;
-  }); 
 
-  it("Deve lançar exceçao quando numero de dados for diferente de 5", () => {
-    expect(() => _sut.pontuar([1,2,3,4], Regra.uns)).toThrow();
+  beforeEach(() => {
+    _sut = new Yahtzee();
   });
 
-  
+  it("Deve lançar exceçao quando numero de dados for diferente de 5", () => {
+    expect(() => _sut.pontuar([1, 2, 3, 4], Regra.uns)).toThrow();
+  });
+
   it("Deve retornar 1 quando a regra escolhida seja uns", () => {
-    expect(_sut.pontuar([1,2,3,4,5], Regra.uns)).toBe(1);
+    expect(_sut.pontuar([1, 2, 3, 4, 5], Regra.uns)).toBe(1);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 1 quando a regra escolhida seja uns", () => {
-    expect(_sut.pontuar([1,2,1,4,1], Regra.uns)).toBe(3);
+    expect(_sut.pontuar([1, 2, 1, 4, 1], Regra.uns)).toBe(3);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 2 quando a regra escolhida seja dois", () => {
-    expect(_sut.pontuar([1,2,1,2,1], Regra.dois)).toBe(4);
+    expect(_sut.pontuar([1, 2, 1, 2, 1], Regra.dois)).toBe(4);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 3 quando a regra escolhida seja tres", () => {
-    expect(_sut.pontuar([1,3,1,3,3], Regra.tres)).toBe(9);
+    expect(_sut.pontuar([1, 3, 1, 3, 3], Regra.tres)).toBe(9);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 4 quando a regra escolhida seja quatros", () => {
-    expect(_sut.pontuar([1,4,3,4,4], Regra.quatros)).toBe(12);
+    expect(_sut.pontuar([1, 4, 3, 4, 4], Regra.quatros)).toBe(12);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 5 quando a regra escolhida seja cincos", () => {
-    expect(_sut.pontuar([5,4,3,5,4], Regra.cincos)).toBe(10);
+    expect(_sut.pontuar([5, 4, 3, 5, 4], Regra.cincos)).toBe(10);
   });
 
   it("Deve retornar a soma de todos os dados com valor igual a 6 quando a regra escolhida seja seis", () => {
-    expect(_sut.pontuar([5,6,3,6,6], Regra.seis)).toBe(18);
+    expect(_sut.pontuar([5, 6, 3, 6, 6], Regra.seis)).toBe(18);
   });
 
   it("Deve retornar a soma de um par de numeros dentro da combinacao", () => {
-    expect(_sut.pontuar([1,2,3,5,5], Regra.par)).toBe(10);
-    expect(_sut.pontuar([6,6,3,2,5], Regra.par)).toBe(12);
-    expect(_sut.pontuar([6,5,3,2,5], Regra.par)).toBe(10);
-    expect(_sut.pontuar([6,5,5,2,5], Regra.par)).toBe(10);
-    expect(_sut.pontuar([1,1,1,2,5], Regra.par)).toBe(2);
-    expect(_sut.pontuar([6,4,2,2,5], Regra.par)).toBe(4);
-    expect(_sut.pontuar([6,5,3,2,3], Regra.par)).toBe(6);
-    expect(_sut.pontuar([4,4,4,2,5], Regra.par)).toBe(8);
-    expect(_sut.pontuar([4,2,1,3,5], Regra.par)).toBe(0);
+    expect(_sut.pontuar([1, 2, 3, 5, 5], Regra.par)).toBe(10);
+    expect(_sut.pontuar([6, 6, 3, 2, 5], Regra.par)).toBe(12);
+    expect(_sut.pontuar([6, 5, 3, 2, 5], Regra.par)).toBe(10);
+    expect(_sut.pontuar([6, 5, 5, 2, 5], Regra.par)).toBe(10);
+    expect(_sut.pontuar([1, 1, 1, 2, 5], Regra.par)).toBe(2);
+    expect(_sut.pontuar([6, 4, 2, 2, 5], Regra.par)).toBe(4);
+    expect(_sut.pontuar([6, 5, 3, 2, 3], Regra.par)).toBe(6);
+    expect(_sut.pontuar([4, 4, 4, 2, 5], Regra.par)).toBe(8);
+    expect(_sut.pontuar([4, 2, 1, 3, 5], Regra.par)).toBe(0);
   });
 
-  
+  it("Deve retornar a soma de dois pares de numeros dentro da combinacao", () => {
+    expect(_sut.pontuar([1, 1, 2, 3, 3], Regra.doisPares)).toBe(8);
+  });
 
+  it("Deve retornar 0 caso a regra de dois pares for escolhida mas nao ha dois pares dentro da combinacao", () => {
+    expect(_sut.pontuar([1, 1, 2, 4, 3], Regra.doisPares)).toBe(0);
+  });
+
+  it("Deve retornar a soma de um trio de numeros dentro da combinacao", () => {
+    expect(_sut.pontuar([1, 1, 4, 4, 4], Regra.trio)).toBe(12);
+  });
+
+  it("Deve retornar a soma de uma quadra de numeros dentro da combinacao", () => {
+    expect(_sut.pontuar([1, 4, 4, 4, 4], Regra.quadra)).toBe(16);
+  });
+
+  it("Deve retornar 15 pontos quando houver uma sequencia de 1,2,3,4,5", () => {
+    expect(_sut.pontuar([5, 3, 2, 4, 1], Regra.sequenciaMenor)).toBe(15);
+  });
+
+  it("Deve retornar 20 pontos quando houver uma sequencia de 2,3,4,5,6", () => {
+    expect(_sut.pontuar([5, 3, 2, 4, 6], Regra.sequenciaMaior)).toBe(20);
+  });
+
+  it("Deve retornar a soma de todos os dados quando houver um trio e um par e a regra for full house", () => {
+    expect(_sut.pontuar([5, 5, 5, 6, 6], Regra.fullHouse)).toBe(27);
+    expect(_sut.pontuar([5, 5, 5, 5, 6], Regra.fullHouse)).toBe(0);
+  });
+
+  it("Deve retornar 50 pontos quando todos os dados forem iguais", () => {
+    expect(_sut.pontuar([5, 5, 5, 5, 5], Regra.yahtzee)).toBe(50);
+    expect(_sut.pontuar([5, 5, 3, 5, 5], Regra.yahtzee)).toBe(0);
+  });
+
+  it("Deve retornar a soma de todos os dados quando a regra escolhida for chance", () => {
+    expect(_sut.pontuar([5, 5, 5, 5, 5], Regra.chance)).toBe(25);
+    expect(
+      _sut.pontuar(
+        [
+          Math.floor(Math.random() * 6) + 1,
+          Math.floor(Math.random() * 6) + 1,
+          Math.floor(Math.random() * 6) + 1,
+          Math.floor(Math.random() * 6) + 1,
+          Math.floor(Math.random() * 6) + 1,
+        ],
+        Regra.chance
+      )
+    ).toBeGreaterThan(4);
+  });
 });
--- src/yahtzee.ts
+++ src/yahtzee.ts
@@ -6,6 +6,14 @@ export enum Regra {
   cincos,
   seis,
   par,
+  doisPares,
+  trio,
+  quadra,
+  sequenciaMenor,
+  sequenciaMaior,
+  fullHouse,
+  yahtzee,
+  chance,
 }
 
 export class Yahtzee {
@@ -13,41 +21,61 @@ export class Yahtzee {
     if (dados.length != 5) {
       throw new Error("Número de dados diferente de 5!");
     }
-    let simples = [0, 0, 0, 0, 0, 0];
 
-    for (let dado = 0; dado < 5; dado++) {
-      for (let index = 1; index <= 6; index++) {
-        if (dados[dado] === index) {
-          simples[index - 1] += index;
+    let simples = [0, 0, 0, 0, 0, 0].map((valorDaPosicao, index) =>
+      dados
+        .filter((dadoDaPosicao) => dadoDaPosicao === index + 1)
+        .reduce((soma, valorCorrent) => soma + valorCorrent, 0)
+    );
+
+    const regrasSimples = (valorDoDado: number): number =>
+      simples[valorDoDado - 1];
+
+    const encontrarNumerosRepetidos = (vezes: number, exclusao: number = 0): number => {
+      for (let index = 6; index >= 1; index--) {
+        if (index !== exclusao && simples[index - 1] >= index * vezes) {
+          return index;
         }
       }
+      return 0;
+    };
+
+    const encontrarGrupoDeNumeros = (param1: number, param2: number) => {
+      if (encontrarNumerosRepetidos(param1) !== 0 && encontrarNumerosRepetidos(param2, encontrarNumerosRepetidos(param1)) !== 0) {
+        return encontrarNumerosRepetidos(param1) * param1 + encontrarNumerosRepetidos(param2, encontrarNumerosRepetidos(param1)) * param2;
+      }
+      return 0;
+    };
+
+    const somarSequencia = (vetor: number[]) => {
+      if (
+        simples.length === vetor.length &&
+        vetor.every((x, i) => x === simples[i])
+      ) {
+        return simples.reduce((soma, valorCorrente) => soma + valorCorrente, 0);
+      }
+      return 0;
     }
 
-    if (regra === Regra.dois) {
-      return simples[1];
-    }
-    if (regra === Regra.tres) {
-      return simples[2];
-    }
-    if (regra === Regra.quatros) {
-      return simples[3];
-    }
-    if (regra === Regra.cincos) {
-      return simples[4];
-    }
-    if (regra === Regra.seis) {
-      return simples[5];
-    }
-    if (regra === Regra.uns) {
-      return simples[0];
-    }
-    if (regra === Regra.par) {
-      for (let index = 6; index >= 0; index--) {
-        if (simples[index - 1] >= index * 2) {
-          return index * 2;
-        }
-      }
-    }
-    return 0;
+    const  conjuntoDeRegras = {
+      [Regra.uns]: regrasSimples(1),
+      [Regra.dois]: regrasSimples(2),
+      [Regra.tres]: regrasSimples(3),
+      [Regra.quatros]: regrasSimples(4),
+      [Regra.cincos]: regrasSimples(5),
+      [Regra.seis]: regrasSimples(6),
+      [Regra.par]: encontrarNumerosRepetidos(2) * 2,
+      [Regra.trio]: encontrarNumerosRepetidos(3) * 3,
+      [Regra.quadra]: encontrarNumerosRepetidos(4) * 4,
+      [Regra.doisPares]: encontrarGrupoDeNumeros(2, 2),
+      [Regra.fullHouse]: encontrarGrupoDeNumeros(3, 2),
+      [Regra.yahtzee]: encontrarNumerosRepetidos(5) !== 0 ? 50 : 0,
+      [Regra.sequenciaMenor] : somarSequencia([1,2,3,4,5,0]),
+      [Regra.sequenciaMaior] : somarSequencia([0,2,3,4,5,6]),
+      [Regra.chance] : simples.reduce((soma, valorCorrente) => soma + valorCorrente, 0)
+    };
+
+    return conjuntoDeRegras[regra];
+    
   }
 }
